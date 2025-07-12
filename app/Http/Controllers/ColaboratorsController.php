3cb6587ab5d574f458732b9577a44fabf0d1859e
JHenrique <henryque.pc.game@gmail.com>
2025-07-11 22:00:27 -0300

    CRIADO VIEW CONTROLADOR

--- app/Http/Controllers/ColaboratorsController.php
+++ app/Http/Controllers/ColaboratorsController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class ColaboratorsController extends Controller
 {
-    //
+    // view home
+    public function index(): View
+    {
+        return view('colaborators');
+    }
 }
